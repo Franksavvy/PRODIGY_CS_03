f9c81d5bba9d840815f92e7eb4b49f14f8eef251
Franksavvy <collinzfrank97@gmail.com>
2024-09-27 03:00:38 +0100

    First commit: Password complexity checker

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ def password_checker(password):
     errors = []
 
     if len(password) < 8:
-        errors.append("Password should be atleast 8 characters long.")
+        #Checks for password length
+        errors.append("Password should be at least 8 characters long.")
 
     if not re.search("[A-Z]", password):
-        errors.append("Password should contain atleast one uppercase letter.")
+        #checks for uppercase letter
+        errors.append("Password should contain at least one uppercase letter.")
 
     if not re.search("[a-z]",password):
+        #Checks for lowercase letter
         errors.append("Password should contain at least one lowercase letter.")
+
     if not re.search("[0-9]",password):
+        #Checks for a number
         errors.append("Password must contain at least one digit.")
+
     if not re.search("[^a-zA-Z0-9]", password):
+        #Checks for a special character
         errors.append("Password must contain at least special character.")
+        
     return errors
 
 def main():
